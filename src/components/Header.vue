dc86ca60201a7d4783bb2be92ecc9f29ba8d5cd0
MikeTheFyke <mikefyke@hotmail.com>
2020-12-20 21:55:53 -0500

    Started logo styles

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="header-container">
-
+    <div id="logo-container">
+      <div id="logo-sphere">
+        <h1 id="logo-X">X</h1>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,8 +21,27 @@ export default {
 
 #header-container{
   width: 100%;
-  height: 100px;
+  height: 60px;
+  padding: 20px 20px;
   background-color: lightgreen;
+}
+
+#logo-container{
+  width: 300px;
+  height: 60px;
+  background-color: white;
+}
+
+#logo-sphere{
+  height: 60px;
+  width: 60px;
+  border-radius: 30px;
+  background-color: teal;
+  text-align: center;
+}
+
+#logo-X{
+  color: yellow;
 }
 
 </style>
