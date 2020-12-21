04643e572d532aaaa6efb31e3a841b50037f231f
MikeTheFyke <mikefyke@hotmail.com>
2020-12-20 21:50:01 -0500

    Added header vue and styles

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="header-container">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  props: {
+  }
+}
+</script>
+
+
+<style scoped>
+
+#header-container{
+  width: 100%;
+  height: 100px;
+  background-color: lightgreen;
+}
+
+</style>
