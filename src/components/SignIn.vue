51f6049aa48296c2b9f18b616772da45c0745464
MikeTheFyke <mikefyke@hotmail.com>
2020-12-21 21:54:01 -0500

    Started stylizing sign in link and container

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,13 +1,34 @@
 <template>
+<div id="SignIn-Container">
+        <h1 id="SignIn-Link">Sign In</h1>
+        <div id="SignIn-DropDown">
+
+        </div>
+</div>
     
 </template>
 
 <script>
+import TweenMax from 'gsap';
 export default {
-    
+    name: 'SignIn',
+        data(){
+            return{
+
+            }
+        }
 }
 </script>
 
 <style scoped>
+
+#SignIn-Link{
+  color: teal;
+  font-size: 20px;
+  width: 62px;
+  margin: 0px;
+  margin-right: 5px;
+  margin-left: 5px;
+}
 
 </style>
