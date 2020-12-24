cadca5ba7829230c2529c621dd2683df64a997b7
MikeTheFyke <mikefyke@hotmail.com>
2020-12-24 15:58:15 -0500

    redoing banner logic'

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -5,8 +5,6 @@
         <img src="../images/venus-surface.jpg" class="banner-image"/>
         <img src="../images/earth-surface.jpg" class="banner-image"/>
         <img src="../images/moon-surface.png" class="banner-image"/>
-        <button onclick="plusDivs(-1)">&#10094;</button>
-        <button onclick="plusDivs(1)">&#10095;</button>
     </div>    
 </template>
 
@@ -17,22 +15,11 @@ export default {
     data(){
         return{
             slideIndex: 1,
-
         }
     },
+    mounted: function(){
+    },
     methods:{
-        showDivs(n) {
-            var i;
-            var x = document.getElementsByClassName("banner-image");
-            if (n > x.length) {
-                this.slideIndex = 1
-            }
-                if (n < 1) {this.slideIndex = x.length}
-                    for (i = 0; i < x.length; i++) {
-                    x[i].style.display = "none";  
-                }
-            x[this.slideIndex-1].style.display = "block";  
-        }
     }
 }
 </script>
@@ -45,7 +32,7 @@ export default {
 }
 
 .banner-image{
-    display: none;
+    /* display: none; */
     height: 100%;
     width: 100%;
 }
