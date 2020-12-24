5a6efa3437c2271bd4e0c2175ef9571b3550b2ad
MikeTheFyke <mikefyke@hotmail.com>
2020-12-24 12:23:04 -0500

    Started banner logic

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -5,13 +5,35 @@
         <img src="../images/venus-surface.jpg" class="banner-image"/>
         <img src="../images/earth-surface.jpg" class="banner-image"/>
         <img src="../images/moon-surface.png" class="banner-image"/>
+        <button onclick="plusDivs(-1)">&#10094;</button>
+        <button onclick="plusDivs(1)">&#10095;</button>
     </div>    
 </template>
 
 <script>
 
 export default {
-    name: 'Banner'
+    name: 'Banner',
+    data(){
+        return{
+            slideIndex: 1,
+
+        }
+    },
+    methods:{
+        showDivs(n) {
+            var i;
+            var x = document.getElementsByClassName("banner-image");
+            if (n > x.length) {
+                this.slideIndex = 1
+            }
+                if (n < 1) {this.slideIndex = x.length}
+                    for (i = 0; i < x.length; i++) {
+                    x[i].style.display = "none";  
+                }
+            x[this.slideIndex-1].style.display = "block";  
+        }
+    }
 }
 </script>
 
@@ -23,9 +45,7 @@ export default {
 }
 
 .banner-image{
-    position: inherit;
-    top: 0;
-    left: 0;
+    display: none;
     height: 100%;
     width: 100%;
 }
