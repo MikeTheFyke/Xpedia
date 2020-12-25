1451c834e24dbbb4195ae9ff0ac21fe22039f143
MikeTheFyke <mikefyke@hotmail.com>
2020-12-24 22:05:06 -0500

    Adding CheckIn styles

--- src/components/CheckIn.vue
+++ src/components/CheckIn.vue
@@ -1,6 +1,8 @@
 <template>
         <div id="CheckIn-Container">
+            <div id="CheckIn-Header">
 
+            </div>
         </div>
 </template>
 
@@ -27,7 +29,16 @@ export default {
     height: 500px;
     border-radius: 15px;
     margin: 0 5%;
-    background-color: red;
+    background-color: white;
+    box-shadow: 0px 8px 6px 8px #dfe3e6;
+}
+
+#CheckIn-Header{
+    width: 100%;
+    height: 100px;
+    border-bottom: grey;
+    border-bottom-style: solid;
+    border-width: thin;
 }
 
 </style>
