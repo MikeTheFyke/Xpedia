82e0b900bd206ba097c4d66b5a3dbbcbd68489dd
MikeTheFyke <mikefyke@hotmail.com>
2020-12-25 18:21:56 -0500

    Added safety vue into main app vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <Banner />
     <Header />
     <CheckIn />
+    <Safety />
   </div>
 </template>
 
@@ -10,6 +11,7 @@
 import Header from './components/Header.vue'
 import Banner from './components/Banner.vue'
 import CheckIn from './components/CheckIn.vue'
+import Safety from './components/Safety.vue'
 
 export default {
   name: 'App',
@@ -17,6 +19,7 @@ export default {
     Header,
     Banner,
     CheckIn,
+    Safety,
   }
 }
 </script>
