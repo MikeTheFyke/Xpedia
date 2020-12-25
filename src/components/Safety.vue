a02ce9652606ddfe169089d04583a146c4ab6f3b
MikeTheFyke <mikefyke@hotmail.com>
2020-12-25 18:34:33 -0500

    Stylized Safety vue

--- src/components/Safety.vue
+++ src/components/Safety.vue
@@ -2,7 +2,11 @@
     <div id="Safety-Container">
         <h2 id="Safety-Title">Travel Safety during Solar Radiation</h2>
         <h1 id="Safety-SubTitle">Resources to help you travel smart, every step on the way</h1>
-        
+        <div id="Safety-Tips-Container">
+            <div class="Safety-Tip"></div>
+            <div class="Safety-Tip"></div>
+            <div class="Safety-Tip"></div>
+        </div>
     </div>
 </template>
 
@@ -23,7 +27,7 @@ export default {
     width: 90%;
     height: 250px;
     margin: 10px 5%;
-    background-color: red;
+    background-color: transparent;
 }
 
 #Safety-Title{
@@ -38,6 +42,24 @@ export default {
     font-size: 15px;
     color: teal;
     margin: 0px;
+}
+
+#Safety-Tips-Container{
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    height: 300px;
+    margin: 10px 0px;
+}
+
+.Safety-Tip{
+    width: 30%;
+    height: 15vh;
+    background-color: white;
+    border-style: solid;
+    border-width: thin;
+    border-color: #dfe3e6;
+    border-radius: 5px;
 }
 
 </style>
