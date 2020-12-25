555e3d178505d9603cc1ccdb5ea3857d314506df
MikeTheFyke <mikefyke@hotmail.com>
2020-12-25 18:19:08 -0500

    Added safety vue scaffolding

---
+++ src/components/Safety.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="Safety-Container">
+
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'Safety',
+    data(){
+        return{
+        }
+    }    
+}
+</script>
+
+<style scoped>
+
+</style>
