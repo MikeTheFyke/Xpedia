992cce3cfe0aafe647440ded527df2de30c91320
MikeTheFyke <mikefyke@hotmail.com>
2020-12-25 18:27:32 -0500

    Added Safety div subtitle

--- src/components/Safety.vue
+++ src/components/Safety.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="Safety-Container">
-
+        <h2 id="Safety-Title">Travel Safety during Solar Radiation</h2>
+        <h1 id="Safety-SubTitle">Resources to help you travel smart, every step on the way</h1>
+        
     </div>
 </template>
 
@@ -19,9 +21,23 @@ export default {
 
 #Safety-Container{
     width: 90%;
-    height: 300px;
+    height: 250px;
     margin: 10px 5%;
     background-color: red;
+}
+
+#Safety-Title{
+    font-family: "Montserrat";
+    font-size: 25px;
+    color: teal;
+    margin: 0px;
+}
+
+#Safety-SubTitle{
+    font-family: "Montserrat";
+    font-size: 15px;
+    color: teal;
+    margin: 0px;
 }
 
 </style>
