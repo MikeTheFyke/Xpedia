52b123ef1a302de6eb07a6514c34db8b6be10169
MikeTheFyke <mikefyke@hotmail.com>
2020-12-28 19:50:46 -0500

    Starting trip ideas vue

---
+++ src/components/NextTripIdeas.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="TripIdeas-Container">
+    </div>   
+</template>
+
+<script>
+export default {
+    name: 'TripIdeas',
+    data(){
+        return{
+        }
+    }    
+}
+</script>
+
+<style scoped>
+
+</style>
