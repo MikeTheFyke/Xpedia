ee197f08ebfab3adae6c4202398361a70ee81517
MikeTheFyke <mikefyke@hotmail.com>
2020-12-28 19:52:15 -0500

    Started Switch Routine vue

---
+++ src/components/SwitchRoutine.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="Routine-Container">
+    </div>   
+</template>
+
+<script>
+export default {
+    name: 'SwitchRoutine',
+    data(){
+        return{
+        }
+    }    
+}
+</script>
+
+<style scoped>
+
+</style>
