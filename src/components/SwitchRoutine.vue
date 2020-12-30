25f579467ca28da088364c91684b790df26678c1
MikeTheFyke <mikefyke@hotmail.com>
2020-12-29 22:26:12 -0500

    Started stylizing switch routine

--- src/components/SwitchRoutine.vue
+++ src/components/SwitchRoutine.vue
@@ -1,5 +1,11 @@
 <template>
     <div id="Routine-Container">
+        <div id="Routine-Top">
+
+        </div>
+        <div id="Routine-Bottom">
+
+        </div>
     </div>   
 </template>
 
@@ -17,10 +23,31 @@ export default {
 
 #Routine-Container{
     width: 90%;
-    height: 400px;
+    height: 500px;
     margin: 40px 5%;
     border-color: #dfe3e6;
     border-style: solid;
 }
 
+#Routine-Top{
+    width: 100%;
+    height: 55%;
+    border-radius: 15px;
+    border-color: #dfe3e6;
+    border-style: solid;
+    overflow: hidden;
+}
+
+#Routine-Bottom{
+    position: relative;
+    top: -15%;
+    width: 90%;
+    height: 55%;
+    margin: 0 5%;
+    border-radius: 15px;
+    border-color: #dfe3e6;
+    background-color: white;
+    border-style: solid;
+    overflow: hidden;
+}
 </style>
