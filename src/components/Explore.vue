e07a4b40075197d43255a49a8ce7bd1840ddf9fa
MikeTheFyke <mikefyke@hotmail.com>
2020-12-30 21:54:36 -0500

    Added poster to explore container

--- src/components/Explore.vue
+++ src/components/Explore.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="Explore-Container">
-
+    <img src="../images/saturn-surface.png" id="Explore-Banner"/>
     </div>
 </template>
 
@@ -22,6 +22,11 @@ export default {
     border-color: #98a3a3;
     border-style: solid;
     margin: 10px 5%;
+}
+
+#Explore-Banner{
+    width: 100%;
+    height: 100%;
 }
 
 </style>
