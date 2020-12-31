63a525ca2ceb44a802a061e30dcd1255baaed28e
MikeTheFyke <mikefyke@hotmail.com>
2020-12-30 20:59:11 -0500

    Added scaffolding to explore vue

---
+++ src/components/Explore.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="Explore-Container">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Explore',
+    data(){
+        return{
+        }
+    }    
+}
+</script>
+
+<style scoped>
+
+</style>
