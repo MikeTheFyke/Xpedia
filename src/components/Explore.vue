1dc1b6e5e7898f0bd0610d9dee4111d7f1021904
MikeTheFyke <mikefyke@hotmail.com>
2020-12-30 21:56:42 -0500

    Added explore div for Explore Poster

--- src/components/Explore.vue
+++ src/components/Explore.vue
@@ -1,6 +1,8 @@
 <template>
     <div id="Explore-Container">
-    <img src="../images/saturn-surface.png" id="Explore-Banner"/>
+        <div id="Explore-Poster">
+            <img src="../images/saturn-surface.png" id="Explore-Banner"/>
+        </div>
     </div>
 </template>
 
@@ -19,9 +21,14 @@ export default {
 #Explore-Container{
     width: 90%;
     height: 350px;
-    border-color: #98a3a3;
-    border-style: solid;
     margin: 10px 5%;
+}
+
+#Explore-Poster{
+    width:100%;
+    height: 300px;
+    border-radius: 15px;
+    overflow: hidden;
 }
 
 #Explore-Banner{
