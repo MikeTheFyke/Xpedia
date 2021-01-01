716de6023b8bca731a6bac971d4dc92fcc69d9c8
MikeTheFyke <mikefyke@hotmail.com>
2021-01-01 17:32:52 -0500

    Added scaffolding to Help vue

---
+++ src/components/Help.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="Help-Container">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Help',
+    data(){
+        return{
+        }
+    }    
+}
+</script>
+
+<style scoped>
+
+#Help-Container{
+
+}
+
+</style>
