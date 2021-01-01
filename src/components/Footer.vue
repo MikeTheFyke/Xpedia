7d33ceef5288e83b70f0892f3c962b1c41e3a3ef
MikeTheFyke <mikefyke@hotmail.com>
2020-12-31 19:59:19 -0500

    Added options container and options link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,9 @@
 <template>
     <div id="Footer-Container">
-        
+        <div id="Options-Container"> 
+            <h1 id="Options-Header">Explore a world of travel with Xpedia</h1>
+            <a href="" id="Options-Link">See our travel options</a>
+        </div>
     </div>
 </template>
 
@@ -21,6 +24,23 @@ export default {
     width: 100%;
     border-color: #98a3a3;
     border-style: solid;
+}
+
+#Options-Container{
+    text-align: center;
+}
+
+#Options-Header{
+    color: teal;
+    font-family: "Montserrat";
+    font-size: 15px;
+}
+
+#Options-Link{
+    color: teal;
+    font-family: "Montserrat";
+    font-size: 12px;
+    outline: none;
 }
 
 </style>
