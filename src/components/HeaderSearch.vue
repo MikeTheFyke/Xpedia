ffbdf7a8b5a1b303f8fcba88cf05dd0eb5b3a4e5
MikeTheFyke <mikefyke@hotmail.com>
2021-01-05 21:27:32 -0500

    Ading styles to search items

--- src/components/HeaderSearch.vue
+++ src/components/HeaderSearch.vue
@@ -4,12 +4,12 @@
             <h1 id="search-text">More Travel</h1>   
             <div id="search-subbar" >
                 <div id="Search-Stays" class="search-item" @mouseover="StaysHighlight" @mouseleave="StaysUnlight"><div class="Header-Item-Img"></div><h1 class="subbar-item">Stays</h1></div>
-                <div id="Search-Flights" class="search-item"><div class="Header-Item-Img"></div><h1 class="subbar-item">Flights</h1></div>
-                <div id="Search-Rovers" class="search-item"><div class="Header-Item-Img"></div><h1 class="subbar-item">Rovers</h1></div>
-                <div id="Search-Shuttles" class="search-item"><div class="Header-Item-Img"></div><h1 class="subbar-item">Shuttles</h1></div>
-                <div id="Search-Packages" class="search-item"><div class="Header-Item-Img"></div><h1 class="subbar-item">Packages</h1></div>
-                <div id="Search-Things" class="search-item"><div class="Header-Item-Img"></div><h1 class="subbar-item">Things To Do</h1></div>
-                <div id="Search-Deals" class="search-item"><div class="Header-Item-Img"></div><h1 class="subbar-item">Deals</h1></div>
+                <div id="Search-Flights" class="search-item" @mouseover="FlightsHighlight" @mouseleave="FlightsUnlight"><div class="Header-Item-Img"></div><h1 class="subbar-item">Flights</h1></div>
+                <div id="Search-Rovers" class="search-item" @mouseover="RoversHighlight" @mouseleave="RoversUnlight"><div class="Header-Item-Img"></div><h1 class="subbar-item">Rovers</h1></div>
+                <div id="Search-Shuttles" class="search-item" @mouseover="ShuttlesHighlight" @mouseleave="ShuttlesUnlight"><div class="Header-Item-Img"></div><h1 class="subbar-item">Shuttles</h1></div>
+                <div id="Search-Packages" class="search-item" @mouseover="PackagesHighlight" @mouseleave="PackagesUnlight"><div class="Header-Item-Img"></div><h1 class="subbar-item">Packages</h1></div>
+                <div id="Search-Things" class="search-item" @mouseover="ThingsHighlight" @mouseleave="ThingskUnlight"><div class="Header-Item-Img"></div><h1 class="subbar-item">Things To Do</h1></div>
+                <div id="Search-Deals" class="search-item" @mouseover="DealsHighlight" @mouseleave="DealsUnlight"><div class="Header-Item-Img"></div><h1 class="subbar-item">Deals</h1></div>
             </div>
         </div>
     </div>
