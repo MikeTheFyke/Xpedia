9a6652979f7444e5f77ed171fac95fc7eb7f6a09
MikeTheFyke <mikefyke@hotmail.com>
2021-01-06 21:04:38 -0500

    Added mercury comntainer styles and logic to Solar vue

--- src/components/Solar.vue
+++ src/components/Solar.vue
@@ -2,23 +2,33 @@
     <div id="Solar-Container">
         <h1 id="Solar-Header">Find your next destination with our Solar System</h1>
         <div id="Planet-Container">
+            <div id="mercury-container"><img src="../images/PlanetMaps/mercuryMap.png" id="mercury-map"></div>
             <div id="sun-container"><img src="../images/PlanetMaps/sunMap.png" id="sun-map"></div>
         </div>
     </div>
 </template>
 
 <script>
-import TweenMax  from 'gsap';
+// import TweenMax  from 'gsap';
 
 export default {
     name: 'Solar',
     data(){
         return{
-            sunStatus : false
+            sunStatus : false,
+            mercuryStatus : false,
+            venusStatus : false,
+            terraStatus : false,
+            marsStatus : false,
+            saturnStatus : false,
+            jupiterStatus : false,
+            uranusStatus : false,
+            neptuneStatus : false,
+            plutoStatus : false,
         }
     },
         mounted: function(){
-            TweenMax.to("#sun-container", 0, { y: "-90px", x:"45.5vw", scale: 0.20 })
+            // TweenMax.to("#sun-container", 0, { y: "-90px", x:"45.5vw", scale: 0.20 })
     },
     methods:{
 
@@ -65,5 +75,22 @@ export default {
     height: 100%;
 }
 
+#mercury-container{
+    width: 200px;
+    height: 200px;
+    border-radius: 100px;
+    border-color: black;
+    border-style: solid;
+    border-width: thin;
+    margin: 0 auto;
+    overflow: hidden;
+}
+
+#mercury-map{
+    position: relative;
+    top: 0px;
+    left: -400px;
+    height: 100%;
+}
 
 </style>
