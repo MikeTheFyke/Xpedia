673a635e9a842591b57d8d89facf0dc563cf9dbf
MikeTheFyke <mikefyke@hotmail.com>
2021-01-08 21:48:01 -0500

    Adjusted planet motion speeds

--- src/components/Solar.vue
+++ src/components/Solar.vue
@@ -37,9 +37,9 @@ export default {
         }
     },
         mounted: function(){
-        TweenMax.to("#sun-map", 5, { x: "39vw", repeat: -1, ease: "Linear.easeInOut" });
-        TweenMax.to("#mercury-map", 5, { x: "43.5vw", repeat: -1, ease: "Linear.easeInOut" });
-        TweenMax.to("#venus-map", 5, { x: "44vw", repeat: -1, ease: "Linear.easeInOut" });
+        TweenMax.to("#sun-map", 10, { x: "39vw", repeat: -1, ease: "Linear.easeInOut" });
+        TweenMax.to("#mercury-map", 8, { x: "43.5vw", repeat: -1, ease: "Linear.easeInOut" });
+        TweenMax.to("#venus-map", 7, { x: "44vw", repeat: -1, ease: "Linear.easeInOut" });
 
         var earthMap = new TimelineMax();
         earthMap.to("#earth-map", 5, { x: "500px", repeat: -1, ease: "Linear.easeInOut" });
