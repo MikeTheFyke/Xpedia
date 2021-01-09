64898f910994fe2a7efa9071a40dd7748dbd3ce6
MikeTheFyke <mikefyke@hotmail.com>
2021-01-08 22:00:01 -0500

    Adjusting mercury map to be repsonsive

--- src/components/Solar.vue
+++ src/components/Solar.vue
@@ -38,8 +38,8 @@ export default {
     },
         mounted: function(){
         TweenMax.to("#sun-map", 10, { x: "39vw", repeat: -1, ease: "Linear.easeInOut" });
-        TweenMax.to("#mercury-map", 8, { x: "43.5vw", repeat: -1, ease: "Linear.easeInOut" });
-        TweenMax.to("#venus-map", 7, { x: "44vw", repeat: -1, ease: "Linear.easeInOut" });
+        TweenMax.to("#mercury-map", 8, { x: "73.25px", repeat: -1, ease: "Linear.easeInOut" });
+        TweenMax.to("#venus-map", 7, { x: "400px", repeat: -1, ease: "Linear.easeInOut" });
 
         var earthMap = new TimelineMax();
         earthMap.to("#earth-map", 5, { x: "500px", repeat: -1, ease: "Linear.easeInOut" });
