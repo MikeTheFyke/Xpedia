d8f82ec169a10bff6b95f18fc7b36d60289d40e8
MikeTheFyke <mikefyke@hotmail.com>
2021-01-09 21:00:16 -0500

    Adjusting beta movement to be responsive

--- src/components/Solar.vue
+++ src/components/Solar.vue
@@ -45,9 +45,9 @@ export default {
         earthMap.to("#earth-map", 5, { x: "25vw", repeat: -1, ease: "Linear.easeInOut" });
         
         var terraBeta = TweenMax.timeline( { repeat: -1 });
-            terraBeta.to("#terra-beta", { motionPath: { path:[ { x: 95, y: 5 }, { x: 190, y:0 }, ], curviness: 1 }, duration: 2.5, ease: "Linear.easeInOut" })
+            terraBeta.to("#terra-beta", { motionPath: { path:[ { x: "60vw", y: 5 }, { x: "90vw", y:0 }, ], curviness: 1 }, duration: 2.5, ease: "Linear.easeInOut" })
             .to("#terra-beta", 0, { zIndex: -1})
-            .to("#terra-beta", { motionPath: { path:[{ x: 95, y:5 },{ x: 0, y:0 },],curviness: 1 }, duration: 2.5, ease: "Linear.easeInOut" })
+            .to("#terra-beta", { motionPath: { path:[{ x: "60vw", y:5 },{ x: 0, y:0 },],curviness: 1 }, duration: 2.5, ease: "Linear.easeInOut" })
             .to("#terra-beta", 0, { zIndex: 2 });
     },
     methods:{
