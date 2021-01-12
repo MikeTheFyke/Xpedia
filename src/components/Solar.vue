63132e8150392cab9228149b19c412e83cf3a96f
MikeTheFyke <mikefyke@hotmail.com>
2021-01-11 20:39:48 -0500

    Added urnaus styles

--- src/components/Solar.vue
+++ src/components/Solar.vue
@@ -341,4 +341,19 @@ export default {
     height: 100%;
 }
 
+#uranus-container{
+    width: 10vw;
+    height: 10vw;
+    border-radius: 5vw;
+    margin: 0 auto;
+    overflow: hidden;
+}
+
+#uranus-map{
+    position: relative;
+    top: 0px;
+    left: -20vw;
+    height: 100%;
+}
+
 </style>
