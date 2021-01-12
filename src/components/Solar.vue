e1fc2748df2486b65e7df94a7fd601d835fa29a1
MikeTheFyke <mikefyke@hotmail.com>
2021-01-11 21:32:20 -0500

    Migrated Mars logic into partial mars vue

--- src/components/Solar.vue
+++ src/components/Solar.vue
@@ -7,11 +7,7 @@
             <Uranus />
             <Jupiter />
             <Saturn />
-            <div id="mars-container">
-                <div id="mars-alpha"><img src="../images/PlanetMaps/marsMap.png" id="mars-map"></div>
-                <div id="mars-phobos"><img src="../images/PlanetMaps/phobosMap.png" id="phobos-map" alt=""></div>
-                <div id="mars-deimos"><img src="../images/PlanetMaps/deimosMap.png" id="deimos-map" alt=""></div>
-            </div>
+            <Mars />
             <div id="terra-container">
                 <div id="terra"><img src="../images/PlanetMaps/earthMap.png" id="earth-map" alt=""></div>
                 <div id="terra-beta"><img src="../images/PlanetMaps/moonMap.png" id="moon-map" alt=""></div>
@@ -33,6 +29,7 @@ import Neptune from './partials/Neptune'
 import Uranus from './partials/Uranus'
 import Jupiter from './partials/Jupiter'
 import Saturn from './partials/Saturn'
+import Mars from './partials/Mars'
 
 import Venus from './partials/Venus'
 import Mercury from './partials/Mercury'
@@ -48,6 +45,7 @@ export default {
         Uranus,
         Jupiter,
         Saturn,
+        Mars,
         Venus,
         Mercury,
         Sun,
@@ -55,7 +53,6 @@ export default {
     data(){
         return{
             terraStatus : false,
-            marsStatus : false,
         }
     },
         mounted: function(){
@@ -69,19 +66,6 @@ export default {
             .to("#terra-beta", { motionPath: { path:[{ x: "60vw", y:5 },{ x: 0, y:0 },],curviness: 1 }, duration: 2.5, ease: "Linear.easeInOut" })
             .to("#terra-beta", 0, { zIndex: 2 });
 
-        TweenMax.to("#mars-map", 8, { x: "20vw", repeat: -1, ease: "Linear.easeInOut" });
-
-        var Deimos = TweenMax.timeline( { repeat: -1 });
-            Deimos.to("#mars-deimos", { motionPath: { path:[ { x: "60vw", y: 5 }, { x: "120vw", y:0 }, ], curviness: 1 }, duration: 2.5, ease: "Linear.easeInOut" })
-            .to("#mars-deimos", 0, { zIndex: -1})
-            .to("#mars-deimos", { motionPath: { path:[{ x: "60vw", y:5 },{ x: 0, y:0 },],curviness: 1 }, duration: 2.5, ease: "Linear.easeInOut" })
-            .to("#mars-deimos", 0, { zIndex: 2 });
-
-        var Phobos = TweenMax.timeline( { repeat: -1 });
-            Phobos.to("#mars-phobos", { motionPath: { path:[ { x: "60vw", y: 5 }, { x: "120vw", y:0 }, ], curviness: 1 }, duration: 1.5, ease: "Linear.easeInOut" })
-            .to("#mars-phobos", 0, { zIndex: -1})
-            .to("#mars-phobos", { motionPath: { path:[{ x: "60vw", y:5 },{ x: 0, y:0 },],curviness: 1 }, duration: 1.5, ease: "Linear.easeInOut" })
-            .to("#mars-phobos", 0, { zIndex: 2 });
     },
     methods:{
     }    
@@ -140,58 +124,6 @@ export default {
 }
 
 #moon-map{
-    height: 100%;
-}
-
-#mars-container{
-    width: 15vw;
-    height: 15vw;
-}
-
-#mars-alpha{
-    width: 10vw;
-    height: 10vw;
-    border-radius: 5vw;
-    margin: 0 auto;
-    overflow: hidden;
-}
-
-#mars-map{
-    position: relative;
-    top: 0px;
-    left: -20vw;
-    height: 100%;
-}
-
-#mars-phobos{
-    position: relative;
-    top: -10vw;
-    left: 0px;
-    width: 1vw;
-    height: 1vw;
-    border-radius: 1vw;
-    overflow: hidden;
-}
-
-#phobos-map{
-    position: absolute;
-    top: 0px;
-    height: 100%;
-}
-
-#mars-deimos{
-    position: relative;
-    top: -8vw;
-    left: 0px;
-    width: 2vw;
-    height: 2vw;
-    border-radius: 1vw;
-    overflow: hidden;
-}
-
-#deimos-map{
-    position: absolute;
-    top: 0px;
     height: 100%;
 }
 </style>
---
+++ src/components/partials/Mars.vue
@@ -0,0 +1,92 @@
+<template>
+    <div id="Mars-Vue-Container">
+        <div id="mars-container">
+            <div id="mars-alpha"><img src="../../images/PlanetMaps/marsMap.png" id="mars-map"></div>
+            <div id="mars-phobos"><img src="../../images/PlanetMaps/phobosMap.png" id="phobos-map" alt=""></div>
+            <div id="mars-deimos"><img src="../../images/PlanetMaps/deimosMap.png" id="deimos-map" alt=""></div>
+        </div>
+    </div>    
+</template>
+
+<script>
+import TweenMax  from 'gsap'
+
+export default {
+        name: 'Mars',
+    data(){
+        return{
+            marsStatus : false,
+        }
+    },
+    mounted: function(){
+        TweenMax.to("#mars-map", 8, { x: "20vw", repeat: -1, ease: "Linear.easeInOut" });
+
+        var Deimos = TweenMax.timeline( { repeat: -1 });
+            Deimos.to("#mars-deimos", { motionPath: { path:[ { x: "60vw", y: 5 }, { x: "120vw", y:0 }, ], curviness: 1 }, duration: 2.5, ease: "Linear.easeInOut" })
+            .to("#mars-deimos", 0, { zIndex: -1})
+            .to("#mars-deimos", { motionPath: { path:[{ x: "60vw", y:5 },{ x: 0, y:0 },],curviness: 1 }, duration: 2.5, ease: "Linear.easeInOut" })
+            .to("#mars-deimos", 0, { zIndex: 2 });
+
+        var Phobos = TweenMax.timeline( { repeat: -1 });
+            Phobos.to("#mars-phobos", { motionPath: { path:[ { x: "60vw", y: 5 }, { x: "120vw", y:0 }, ], curviness: 1 }, duration: 1.5, ease: "Linear.easeInOut" })
+            .to("#mars-phobos", 0, { zIndex: -1})
+            .to("#mars-phobos", { motionPath: { path:[{ x: "60vw", y:5 },{ x: 0, y:0 },],curviness: 1 }, duration: 1.5, ease: "Linear.easeInOut" })
+            .to("#mars-phobos", 0, { zIndex: 2 });
+    }
+    
+}
+</script>
+
+<style scoped>
+#mars-container{
+    width: 15vw;
+    height: 15vw;
+}
+
+#mars-alpha{
+    width: 10vw;
+    height: 10vw;
+    border-radius: 5vw;
+    margin: 0 auto;
+    overflow: hidden;
+}
+
+#mars-map{
+    position: relative;
+    top: 0px;
+    left: -20vw;
+    height: 100%;
+}
+
+#mars-phobos{
+    position: relative;
+    top: -10vw;
+    left: 0px;
+    width: 1vw;
+    height: 1vw;
+    border-radius: 1vw;
+    overflow: hidden;
+}
+
+#phobos-map{
+    position: absolute;
+    top: 0px;
+    height: 100%;
+}
+
+#mars-deimos{
+    position: relative;
+    top: -8vw;
+    left: 0px;
+    width: 2vw;
+    height: 2vw;
+    border-radius: 1vw;
+    overflow: hidden;
+}
+
+#deimos-map{
+    position: absolute;
+    top: 0px;
+    height: 100%;
+}
+</style>
